be00eed40f64fbfd7dfc7ec8d0115ff13e7b983b
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2020-08-17 21:59:55 -0300

    Add BankAccountService (adapter inside domain)

---
+++ src/main/java/com/freemanpivo/hexagonal/domain/usecase/BankAccountService.java
@@ -0,0 +1,29 @@
+package com.freemanpivo.hexagonal.domain.usecase;
+
+
+import com.freemanpivo.hexagonal.domain.model.BankAccount;
+import com.freemanpivo.hexagonal.ports.inbound.DepositUseCase;
+import com.freemanpivo.hexagonal.ports.inbound.WithdrawUseCase;
+
+import java.math.BigDecimal;
+
+public class BankAccountService implements DepositUseCase, WithdrawUseCase {
+    @Override
+    public void deposit(Long id, Long amount) {
+        BankAccount account = new BankAccount(1L, 0L); // refactor to find in DB port
+        account.deposit(amount);
+
+        // refactor -> implement save method DB port
+    }
+
+    @Override
+    public boolean withdraw(Long id, Long amount) {
+        BankAccount account = new BankAccount(1L, 0L);
+
+        boolean hasWithdrawn = account.withdraw(amount);
+        if (hasWithdrawn) {
+            // update DB Port
+        }
+        return hasWithdrawn;
+    }
+}
