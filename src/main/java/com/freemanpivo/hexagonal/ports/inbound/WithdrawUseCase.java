4d9a0e03b6ffc8b34fbc3dad70998648295767a2
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2020-08-17 21:58:10 -0300

    [Fix] return type of withdraw method

--- src/main/java/com/freemanpivo/hexagonal/ports/inbound/WithdrawUseCase.java
+++ src/main/java/com/freemanpivo/hexagonal/ports/inbound/WithdrawUseCase.java
@@ -3,5 +3,5 @@ package com.freemanpivo.hexagonal.ports.inbound;
 import java.math.BigDecimal;
 
 public interface WithdrawUseCase {
-    void withdraw(Long id, Long amount);
+    boolean withdraw(Long id, Long amount);
 }
