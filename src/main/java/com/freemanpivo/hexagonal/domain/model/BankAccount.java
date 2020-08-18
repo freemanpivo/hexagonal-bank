55e33a8372de226466b924a7ff15cb527cbc7fed
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2020-08-17 21:58:54 -0300

    Add BankAccount model (domain)

---
+++ src/main/java/com/freemanpivo/hexagonal/domain/model/BankAccount.java
@@ -0,0 +1,23 @@
+package com.freemanpivo.hexagonal.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BankAccount {
+    private Long register;
+    private Long balance; // treating currency as unit
+
+    public boolean withdraw(Long amount) {
+        if (balance < 0) {
+            return false;
+        }
+        this.setBalance(balance - amount);
+        return true;
+    }
+
+    public void deposit(Long amount) {
+        this.setBalance(balance + amount);
+    }
+}
